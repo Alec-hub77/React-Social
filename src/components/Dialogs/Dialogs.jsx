2f97cb853c6cf6667c4a7d516fce951ad7554020
Alec-hub77 <oleg.ostapiuk@yahoo.com>
2021-02-10 18:11:22 +0200

    first commit

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
-import s from './Dialogs.module.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import s from "./Dialogs.module.css";
 
-const Dialogs = () => {
+const DialogItem = (props) => {
+  return (
+    <div className={s.dialog}>
+      <NavLink to={`/dialogs/${props.id}`} activeClassName={s.active}>
+        {props.name}
+      </NavLink>
+    </div>
+  );
+};
+
+const Message = (props) =>{
     return (
-        <div>Dialogs</div>
+        <div className={s.message}>{props.message}</div>
     )
 }
+
+const Dialogs = () => {
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialog_items}>
+        <DialogItem name="Dimych" id="1" />
+        <div className={s.dialog}>
+          <DialogItem name="Andrey" id="2" />
+        </div>
+        <div className={s.dialog}>
+          <DialogItem name="Sveta" id="3" />
+        </div>
+        <div className={s.dialog}>
+          <DialogItem name="Sasha" id="4" />
+        </div>
+        <div className={s.dialog}>
+          <DialogItem name="Victor" id="5" />
+        </div>
+        <div className={s.dialog}>
+          <DialogItem name="Sergey" id="6" />
+        </div>
+      </div>
+      <div className={s.messages}>
+        <Message message='Hello' />
+        <Message message='How are you?' />
+        <Message message='Coronavirus' />
+      </div>
+    </div>
+  );
+};
 
 export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import ContentImg from '../Profile/content-img.jpg'
-import Avatar from '../Profile/avatar.jpg'
-import s from './Profile.module.css';
-import MyPosts from './MyPosts/MyPosts';
+import React from "react";
 
-const Profile = ()=>{
-    return (
-        <div>
-        <div>
-          <img src={ContentImg} className={s.content_img}/>
-        </div>
-        <div>
-          <div>
-            <img src={Avatar} className={s.avatar_img}/>
-          </div>
-        </div>
-        < MyPosts />
-      </div>
-    )
-}
+import s from "./Profile.module.css";
+import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
+
+const Profile = () => {
+  return (
+    <div>
+      <ProfileInfo />
+      <MyPosts />
+    </div>
+  );
+};
 
 export default Profile;
